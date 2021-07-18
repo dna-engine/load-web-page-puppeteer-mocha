663d9910b0564fc759e2356778693f72aae0a3ba
dpilafian <dem@centerkey.com>
2021-07-18 13:10:40 -0700

    Switch to cleaner async/await syntax

--- spec.js
+++ spec.js
@@ -1,37 +1,32 @@
-// Mocha Specification Cases
+// Mocha Specification Suite
 
 // Imports
+import puppeteer from 'puppeteer';
 import { assertDeepStrictEqual } from 'assert-deep-strict-equal';
 import { browserReady } from 'puppeteer-browser-ready';
-import puppeteer from 'puppeteer';
 
 // Setup
-const pageUrl = 'https://pretty-print-json.js.org/';
-const web = {};  //fields: browser, page, response, url, status, statusText, html
-let $;
-const loadWebPage = () => puppeteer.launch()
-   .then(browserReady.goto(pageUrl, { web: web }))
-   .then(() => $ = web.$)
-   .catch(error => console.error(error));
-const closeWebPage = () => browserReady.close(web)
-   .catch(error => console.error(error));
+const url = 'https://pretty-print-json.js.org/';
+let web;  //fields: browser, page, response, status, location, title, html, $
+const loadWebPage =  async () => web = await puppeteer.launch().then(browserReady.goto(url));
+const closeWebPage = async () => await browserReady.close(web);
 before(loadWebPage);
 after(closeWebPage);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 describe('The web page', () => {
 
-   it('has the correct URL -> ' + pageUrl, () => {
-      const actual =   { url: web.response.url() };
-      const expected = { url: pageUrl };
+   it('has the correct URL -> ' + url, () => {
+      const actual =   { status: web.status, url: web.location.href };
+      const expected = { status: 200,        url: url };
       assertDeepStrictEqual(actual, expected);
       });
 
    it('has exactly one header, main, and footer', () => {
       const actual =   {
-         header: $('body >header').length,
-         main:   $('body >main').length,
-         footer: $('body >footer').length,
+         header: web.$('body >header').length,
+         main:   web.$('body >main').length,
+         footer: web.$('body >footer').length,
          };
       const expected = { header: 1, main: 1, footer: 1 };
       assertDeepStrictEqual(actual, expected);
